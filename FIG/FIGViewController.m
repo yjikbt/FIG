afe151865d030540fe65be7b04d292313626754d
yjikbt <yjikbt@gmail.com>
2014-09-04 21:07:21 +0900

    リポジトリ作成

--- FIG/FIGViewController.m
+++ FIG/FIGViewController.m
@@ -15,37 +15,15 @@
 {
     [super viewDidLoad];
 
-    // Configure the view.
+    // SKView取得
     SKView * skView = (SKView *)self.view;
-    skView.showsFPS = YES;
-    skView.showsNodeCount = YES;
     
-    // Create and configure the scene.
+    // 初期シーン作成
     SKScene * scene = [FIGMyScene sceneWithSize:skView.bounds.size];
     scene.scaleMode = SKSceneScaleModeAspectFill;
     
-    // Present the scene.
+    // シーン貼り付け
     [skView presentScene:scene];
-}
-
-- (BOOL)shouldAutorotate
-{
-    return YES;
-}
-
-- (NSUInteger)supportedInterfaceOrientations
-{
-    if ([[UIDevice currentDevice] userInterfaceIdiom] == UIUserInterfaceIdiomPhone) {
-        return UIInterfaceOrientationMaskAllButUpsideDown;
-    } else {
-        return UIInterfaceOrientationMaskAll;
-    }
-}
-
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Release any cached data, images, etc that aren't in use.
 }
 
 @end
